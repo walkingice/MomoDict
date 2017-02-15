b74f874af1147e3a7408ec894516a14f62887f34
Julian_Chu <walkingice0204@gmail.com>
2017-02-15 00:48:53 -0800

    ui: add actionbar
    Also move button function into menu.

--- app/src/main/java/org/zeroxlab/momodict/MainActivity.java
+++ app/src/main/java/org/zeroxlab/momodict/MainActivity.java
@@ -3,18 +3,18 @@ package org.zeroxlab.momodict;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = Momodict.TAG;
 
     static final int REQ_CODE_IMPORT = 0x1002;
-
-    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +24,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        mButton = (Button) findViewById(R.id.btn_1);
-        mButton.setOnClickListener((v) -> {
-            Intent i = new Intent();
-            i.setClass(this, FileImportActivity.class);
-            startActivityForResult(i, REQ_CODE_IMPORT);
-        });
+        initActionBar();
+    }
+
+    private void initActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.actionbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, R.id.menu_import, Menu.NONE, "Import dictionary");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_import:
+                onImportClicked();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -39,6 +55,12 @@ public class MainActivity extends AppCompatActivity {
                 onResultImport(resultCode, data);
                 break;
         }
+    }
+
+    private void onImportClicked() {
+        Intent i = new Intent();
+        i.setClass(this, FileImportActivity.class);
+        startActivityForResult(i, REQ_CODE_IMPORT);
     }
 
     private void onResultImport(int resultCode, Intent data) {
