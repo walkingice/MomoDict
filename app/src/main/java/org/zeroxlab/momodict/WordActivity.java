64ead39fc6e6e5d41832b1f05556f4e3b0e971c0
Julian_Chu <walkingice0204@gmail.com>
2017-02-22 03:35:05 +0800

    ui: add icon to toolbar and set title

--- app/src/main/java/org/zeroxlab/momodict/WordActivity.java
+++ app/src/main/java/org/zeroxlab/momodict/WordActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import org.zeroxlab.momodict.ui.WordFragment;
 
@@ -21,7 +23,16 @@ public class WordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_with_one_fragment);
         setFragments();
+        initActionBar();
     }
+
+    private void initActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.actionbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getIntent().getStringExtra(Momodict.EXTRA_DATA_1));
+    }
+
 
     private void setFragments() {
         final WordFragment wordFrg = new WordFragment();
