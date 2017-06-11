5b5019bcf983d392cfde411cf7d14c98e00ad315
Julian_Chu <walkingice0204@gmail.com>
2017-06-11 22:10:58 +0800

    ui: basic implementation for displaying imported dictionaries

--- app/src/main/java/org/zeroxlab/momodict/ui/DictListFragment.kt
+++ app/src/main/java/org/zeroxlab/momodict/ui/DictListFragment.kt
@@ -2,14 +2,28 @@ package org.zeroxlab.momodict.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.zeroxlab.momodict.Controller
 import org.zeroxlab.momodict.R
+import org.zeroxlab.momodict.widget.DictionaryRowPresenter
+import org.zeroxlab.momodict.widget.SelectorAdapter
+import org.zeroxlab.momodict.widget.SelectorAdapter.Type
 
 class DictListFragment : Fragment() {
 
+    lateinit var mCtrl: Controller
     lateinit var mBtnImport: View
+    lateinit var mList: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mCtrl = Controller(activity!!)
+    }
 
     override fun onCreateView(inflater: LayoutInflater?,
                               container: ViewGroup?,
@@ -17,6 +31,19 @@ class DictListFragment : Fragment() {
         val frgView = inflater!!.inflate(R.layout.fragment_dictionaries_list, container, false)
         mBtnImport = frgView.findViewById(R.id.btn_1).also {
             it.setOnClickListener({ onImportClicked() })
+        }
+
+        mList = (frgView.findViewById(R.id.list) as RecyclerView).also {
+            val decoration = DividerItemDecoration(context,
+                    (it.layoutManager as LinearLayoutManager).orientation)
+            it.addItemDecoration(decoration)
+
+            val map = HashMap<Type, SelectorAdapter.Presenter<*>>()
+            map.put(SelectorAdapter.Type.A, DictionaryRowPresenter())
+
+            it.adapter = SelectorAdapter(map).also { adapter ->
+                mCtrl.books.forEach { book -> adapter.addItem(book.bookName, Type.A) }
+            }
         }
         return frgView
     }
