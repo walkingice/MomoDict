d1e4234d80dcd4735ec6b6bad3d7e767a284fa65
Julian_Chu <walkingice0204@gmail.com>
2018-05-10 23:26:51 +0800

    refactor HistoryRowPresenter
    use my preferred way to refactor HistoryRowPresenter

--- app/src/main/java/org/zeroxlab/momodict/widget/HistoryRowPresenter.kt
+++ app/src/main/java/org/zeroxlab/momodict/widget/HistoryRowPresenter.kt
@@ -9,40 +9,36 @@ import android.widget.TextView
 import org.zeroxlab.momodict.R
 import org.zeroxlab.momodict.model.Record
 
-class HistoryRowPresenter(internal var mListener: View.OnClickListener,
-                          internal var mLongListener: View.OnLongClickListener) : SelectorAdapter.Presenter<Record> {
+class HistoryRowPresenter(private var clickCb: (v: View) -> Unit,
+                          private var longClickCb: (v: View) -> Boolean) : SelectorAdapter.Presenter<Record> {
 
     override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val viewGroup = inflater.inflate(
-                R.layout.history_row, parent, false) as ViewGroup
-        return InnerViewHolder(viewGroup)
+        return LayoutInflater.from(parent.context)
+                .let { it.inflate(R.layout.history_row, parent, false) }
+                .let { InnerViewHolder(it) }
     }
 
     override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, item: Record) {
-        val holder = viewHolder as InnerViewHolder
-        holder.iText1.text = item.wordStr
-        holder.iText2.text = item.count.toString() + ""
-        holder.itemView.setOnClickListener { view ->
-            view.tag = item.wordStr
-            mListener.onClick(view)
-        }
-        holder.itemView.setOnLongClickListener { view ->
-            view.tag = item.wordStr
-            mLongListener.onLongClick(view)
-            true
+        with(viewHolder as InnerViewHolder) {
+            iText1.text = item.wordStr
+            iText2.text = item.count.toString() + ""
+            itemView.setOnClickListener { view ->
+                view.tag = item.wordStr
+                clickCb(view)
+            }
+
+            itemView.setOnLongClickListener { view ->
+                view.tag = item.wordStr
+                longClickCb(view)
+                true
+            }
         }
     }
 
     override fun onUnbindViewHolder(viewHolder: RecyclerView.ViewHolder) {}
 
     internal inner class InnerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var iText1: TextView
-        var iText2: TextView
-
-        init {
-            iText1 = view.findViewById<View>(R.id.text_1) as TextView
-            iText2 = view.findViewById<View>(R.id.text_2) as TextView
-        }
+        var iText1: TextView = view.findViewById<View>(R.id.text_1) as TextView
+        var iText2: TextView = view.findViewById<View>(R.id.text_2) as TextView
     }
 }
